fce7a683b5248b35111abacfbf4ed7de6d695202
460N1 <agon.hoxha@uni-pr.edu>
2019-06-20 20:50:32 +0200

    Welcome Activity - terms and conditions - options (just the frame)

---
+++ app/src/main/java/com/a60n1/kohabarna/WelcomeActivity.kt
@@ -0,0 +1,61 @@
+package com.a60n1.kohabarna
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        val prefs: SharedPreferences = this.getSharedPreferences("com.a60n1.kohabarna", Context.MODE_PRIVATE)
+        txtWelcome.visibility = View.INVISIBLE
+        mainLayout.visibility = View.INVISIBLE
+        if (!prefs.contains("1stTime")) {
+            fadeItems(pillsIMG, txtWelcome, mainLayout)
+            btnTandC.setOnClickListener {
+                if (chxAccepted.isChecked) {
+                    prefs.edit().putBoolean("1stTime", false).apply()
+                    startActivity(Intent(this, IntroActivity::class.java))
+                    finish()
+                    //Toast.makeText(this@MainActivity,prefs.getBoolean("firstTime",true).toString(),Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this@WelcomeActivity, getString(R.string.not_accepted), Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            pillsIMG.visibility = View.INVISIBLE
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun fadeItems(img: ImageView, txt: TextView, lin: LinearLayout) {
+        val fadeOut = AlphaAnimation(1f, 0f)
+        fadeOut.interpolator = AccelerateInterpolator()
+        fadeOut.duration = 1000
+        fadeOut.setAnimationListener(object : AnimationListener {
+            override fun onAnimationEnd(animation: Animation) {
+                img.visibility = View.GONE
+                txt.visibility = View.VISIBLE
+                lin.visibility = View.VISIBLE
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+            override fun onAnimationStart(animation: Animation) {}
+        })
+        img.startAnimation(fadeOut)
+    }
+}
